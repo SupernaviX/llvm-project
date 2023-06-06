7a83e01e8adc01ba82f451a9fdf8a9afdd271a45
Simon Gellis <simongellis@gmail.com>
2023-06-06 17:58:15 -0400

    Disassembler already works

--- llvm/lib/Target/V810/Disassembler/V810Disassembler.cpp
+++ llvm/lib/Target/V810/Disassembler/V810Disassembler.cpp
@@ -35,11 +35,69 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeV810Disassembler() {
                                          createV810Disassembler);
 }
 
+static const unsigned GenRegDecoderTable[] = {
+  V810::R0,   V810::R1,   V810::R2,   V810::R3,
+  V810::R4,   V810::R5,   V810::R6,   V810::R7,
+  V810::R8,   V810::R9,   V810::R10,  V810::R11,
+  V810::R12,  V810::R13,  V810::R14,  V810::R15,
+  V810::R16,  V810::R17,  V810::R18,  V810::R19,
+  V810::R20,  V810::R21,  V810::R22,  V810::R23,
+  V810::R24,  V810::R25,  V810::R26,  V810::R27,
+  V810::R28,  V810::R29,  V810::R30,  V810::R31 };
+
+static const unsigned SysRegDecoderTable[] = {
+  V810::SR0,  V810::SR1,  V810::SR2,  V810::SR3,
+  V810::SR4,  V810::SR5,  V810::SR6,  V810::SR7,
+  0,          0,          0,          0,
+  0,          0,          0,          0,
+  0,          0,          0,          0,
+  0,          0,          0,          0,
+  V810::SR24, V810::SR25, 0,          0,
+  0,          V810::SR29, V810::SR30, V810::SR31 };
+
+static DecodeStatus DecodeGenRegsRegisterClass(MCInst &Inst, unsigned RegNo,
+                                               uint64_t Address,
+                                               const MCDisassembler *Decoder) {
+  if (RegNo > 31)
+    return MCDisassembler::Fail;
+  unsigned Reg = GenRegDecoderTable[RegNo];
+  Inst.addOperand(MCOperand::createReg(Reg));
+  return MCDisassembler::Success;
+}
+
+static DecodeStatus DecodeSysRegsRegisterClass(MCInst &Inst, unsigned RegNo,
+                                               uint64_t Address,
+                                               const MCDisassembler *Decoder) {
+  if (RegNo > 31)
+    return MCDisassembler::Fail;
+  unsigned Reg = SysRegDecoderTable[RegNo];
+  if (Reg == 0)
+    return MCDisassembler::Fail;
+  Inst.addOperand(MCOperand::createReg(Reg));
+  return MCDisassembler::Success;
+}
+
 #include "V810GenDisassemblerTables.inc"
 
 DecodeStatus V810Disassembler::getInstruction(MCInst &Instr, uint64_t &Size,
                                               ArrayRef<uint8_t> Bytes,
                                               uint64_t Address,
                                               raw_ostream &CStream) const {
+  uint16_t SmallInsn = support::endian::read16le(Bytes.data());
+  DecodeStatus Result = decodeInstruction(DecoderTableV81016, Instr, SmallInsn, Address,
+                                          this, STI);
+  if (Result != DecodeStatus::Fail) {
+    Size = 2;
+    return Result;
+  }
+
+  uint32_t LargeInsn = support::endian::read32le(Bytes.data());
+  Result = decodeInstruction(DecoderTableV81032, Instr, LargeInsn, Address,
+                             this, STI);
+  if (Result != DecodeStatus::Fail) {
+    Size = 4;
+    return Result;
+  }
+
   return DecodeStatus::Fail;
 }
