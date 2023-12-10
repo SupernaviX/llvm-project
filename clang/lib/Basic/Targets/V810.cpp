5502725130c8d2e299f9cb73b1d3f5f37510b5c0
PizzaRollsRoyce <me@pizzarollsroyce.com>
2023-12-09 19:38:54 -0600

    Add PSW to list of possible clobbers for asm()
    "cc" was being silently ignored, and it was resulting in inline assembly
    possibly corrupting registers without the compiler knowing about it.
    
    "psw" should be used instead, and this is compatible with the V810 GCC 4.6 patches

--- clang/lib/Basic/Targets/V810.cpp
+++ clang/lib/Basic/Targets/V810.cpp
@@ -8,7 +8,7 @@ const char *const V810TargetInfo::GCCRegNames[] = {
     // Integer registers
     "r0",  "r1",  "r2",  "r3",  "r4",  "r5",  "r6",  "r7",  "r8",  "r9",  "r10",
     "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18", "r19", "r20", "r21",
-    "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31",
+    "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31", "sr5",
 };
 
 ArrayRef<const char *> V810TargetInfo::getGCCRegNames() const {
@@ -17,7 +17,7 @@ ArrayRef<const char *> V810TargetInfo::getGCCRegNames() const {
 
 const TargetInfo::GCCRegAlias V810TargetInfo::GCCRegAliases[] = {
     {{"hp"}, "r2"},  {{"sp"}, "r3"},  {{"gp"}, "r4"}, {{"tp"}, "r5"},
-    {{"lp"}, "r31"},
+    {{"lp"}, "r31"}, {{"cc", "psw"}, "sr5"}
 };
 
 ArrayRef<TargetInfo::GCCRegAlias> V810TargetInfo::getGCCRegAliases() const {
